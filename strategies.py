72e04891a9524e5ac78143c5100840bb3ebd359e
Nanofication <nansquared@gmail.com>
2021-08-22 17:47:55 -0400

    Refactored code layout to match new flow

--- strategies.py
+++ strategies.py
@@ -1,120 +1,11 @@
 from pandas_datareader import data
 from pandas_datareader._utils import RemoteDataError
 from pandas.tseries.offsets import BDay
-from utils import math_calcs, stock_utils
+from utils import stock_utils
 import pandas as pd
 import numpy as np
 import datetime
 import mplfinance as fplt
-
-def getTDData(ticker,start,end):
-    try:
-        stockData = stock_utils.getStockDataTD(ticker,
-                                               datetime.datetime.strptime(start, '%Y-%m-%d'),
-                                               datetime.datetime.strptime(end, '%Y-%m-%d'))
-
-        stockData.insert(loc=0, column='Counter', value=np.arange(len(stockData)))
-        stockData = stockData.reset_index()
-        stockData = stockData.astype({'Date': 'datetime64'})
-        stockData = stockData.set_index('Date')
-        stockData = stockData.sort_values(by='Date')
-        return stockData
-    except RemoteDataError:
-        print('No Data found for {0}'.format(ticker))
-
-def getData(ticker, start, end):
-    try:
-        stockData = data.DataReader(ticker,
-                                    'stooq',
-                                    start,
-                                    end)
-
-        stockData.insert(loc=0, column='Counter', value=np.arange(len(stockData)))
-        stockData = stockData.reset_index()
-        stockData = stockData.astype({'Date': 'datetime64'})
-        stockData = stockData.set_index('Date')
-        stockData = stockData.sort_values(by='Date')
-        return stockData
-    except RemoteDataError:
-        print('No Data found for {0}'.format(ticker))
-
-def getPivotPoints(df):
-    """
-    Get pivot points in stock chart
-    Swing lows are reversals when the bars go from red to green
-    Swing highs are reversals when bars go from green to red
-    :param df: Stock Chart
-    :return:
-    """
-    resistancePivots = []
-    supportPivots = []
-    prevBarIsGreen = df['Close'].iloc[0] > df['Open'].iloc[0]
-    prevClose = df['Close'].iloc[0]
-    print(prevBarIsGreen)
-
-    for index, row in df.iloc[1:].iterrows():
-        if row['Close'] > row['Open'] and prevBarIsGreen == False:
-            num = prevClose if prevClose < row['Open'] else row['Open']
-            supportPivots.append((index.to_pydatetime(),num, row['Counter'])) # Note: You could have prev red candle as start of pivot (OR?)
-            prevBarIsGreen = True
-        elif row['Close'] < row['Open'] and prevBarIsGreen:
-            num = prevClose if prevClose > row['Open'] else row['Open']
-            resistancePivots.append((index.to_pydatetime(),num, row['Counter']))
-            prevBarIsGreen = False
-        prevClose = row['Close']
-
-    return supportPivots, resistancePivots
-
-def generateTrendLine(pivots, startTime=0, endTime=0, reverse=False):
-    """
-    Sort the pivots by ascending or descending order
-    Graph a line between the 2 points and check how many pivot points touch it
-    Count how many range exceeds or is close enough
-    1. Ignore dates outside of pivot range
-    :param pivots:
-    :param startTime:
-    :param endTime:
-    :return:
-    """
-    sortedPivots = sorted(pivots,key=lambda x:x[1],reverse=reverse)
-    p1 = sortedPivots[0]
-    prevDate = p1[0]
-    highScore = 0
-    pair = {}
-
-    for piv in sortedPivots[1:]:
-        if not (prevDate > piv[0]):
-            trendPoint = Trendline(p1, piv, sortedPivots).calcTouchPoints()
-            if highScore < trendPoint:
-                highScore = trendPoint
-                pair = {
-                        'Date1': p1[0], # .strftime("%Y-%m-%d"),
-                        'Pivot1': p1[1],
-                        'Date2': piv[0], # .strftime("%Y-%m-%d"),
-                        'Pivot2': piv[1]
-                    }
-                prevDate = pair['Date2']
-
-    return pair
-
-class Trendline():
-    def __init__(self, startPivot, endPivot, pivots):
-        self.startPivot = startPivot
-        self.endPivot = endPivot
-        self.pivots = pivots
-        self.slope, self.yIntercept = math_calcs.getLineGraph(startPivot[2], startPivot[1], endPivot[2], endPivot[1])
-        self.marginOfError = 0.1
-        self.score = 0
-
-    def calcTouchPoints(self):
-        touchPoint = 0
-        for p in self.pivots:
-            if abs(self.yIntercept + self.slope * p[2] - p[1]) < ((self.yIntercept + self.slope * p[2]) * self.marginOfError):
-                touchPoint += 1
-
-        self.score = touchPoint
-
-        return touchPoint
 
 class MATrading:
     """
@@ -135,7 +26,7 @@ class MATrading:
         start = start.strftime('%Y-%m-%d')
         end = self.endDate.strftime('%Y-%m-%d')
 
-        return getTDData(self.ticker, start, end)
+        return stock_utils.getTDData(self.ticker, start, end)
 
     def generateMAData(self,stockData=None):
         df = stockData if stockData else self.generateStockDate()
@@ -177,9 +68,9 @@ class MATrading:
         endCounter = df[df['Date'] == endDate]['index'][0] - 1
 
         stockData = df.iloc[startCounter:endCounter]
-        up, down = getPivotPoints(stockData)
+        up, down = stock_utils.getPivotPoints(stockData)
 
-        pair = generateTrendLine(down, reverse=True)
+        pair = stock_utils.generateTrendLine(down, reverse=True)
 
         trendLine = [(pair['Date1'].strftime("%Y-%m-%d"), pair['Pivot1']),
                      (pair['Date2'].strftime("%Y-%m-%d"), pair['Pivot2'])]
@@ -208,7 +99,7 @@ class EMACrossoverTrading:
         start = start.strftime('%Y-%m-%d')
         end = self.endDate.strftime('%Y-%m-%d')
 
-        return getTDData(self.ticker, start, end)
+        return stock_utils.getTDData(self.ticker, start, end)
 
     def generateEMAData(self, stockData=None):
         df = stockData if not stockData.empty else self.generateStockDate()
@@ -297,11 +188,11 @@ if __name__ == '__main__':
     stock = 'NIO'
     ma = MATrading(stock, datetime.date(2020,1,2), datetime.date(2021,6,3))
     stockData = ma.generateMAData()
-    up, down = getPivotPoints(stockData)
+    up, down = stock_utils.getPivotPoints(stockData)
 
     dates = ma.findCloseNearMa(stockData)
-    startDate = ma.findDateHighestPastPoint(stockData,dates[5])
-    trendLine = ma.generateLocalTrendLine(stockData, startDate, dates[5])
+    startDate = ma.findDateHighestPastPoint(stockData,dates[10])
+    trendLine = ma.generateLocalTrendLine(stockData, startDate, dates[10])
     # print(up)
     # pair = generateTrendLine(down, reverse=True)
     #
--- utils/stock_utils.py
+++ utils/stock_utils.py
@@ -1,6 +1,8 @@
 from pandas_datareader import data
 from pandas_datareader._utils import RemoteDataError
 from pandas.tseries.offsets import BDay
+from finviz.screener import Screener
+from utils import math_calcs
 from dask import delayed
 from config.configuration import TD_API
 import pandas
@@ -44,7 +46,46 @@ def convertData(data):
 
 ###
 
-def getStockDataTD(symbol, startDate, endDate):
+def getDailyDataTD(symbol, startDate, endDate):
+    priceHistory = "https://api.tdameritrade.com/v1/marketdata/{symbol}/pricehistory".format(symbol=symbol)
+    dailyChartPayLoad = {
+        'apikey': TD_API['API_KEY'],
+        'periodType': 'day',
+        'frequencyType': 'minute',
+        'frequency': '1',
+        'period': '1',
+        'endDate': str(unixTimeMs(endDate)),
+        'startDate': str(unixTimeMs(startDate)),
+        'needExtendedHoursData': 'true'
+    }
+    content = requests.get(url=priceHistory, params=dailyChartPayLoad)
+    data = content.json()
+    data = convertData(data)
+    data = pandas.DataFrame(data['candles'])
+    data.columns = data.columns.str.capitalize()
+    return data
+
+def splitCandles(data):
+    """
+    Split the daily set of candle for the given stock into premarket, regular market and after hours
+    :param data: Json data of candles
+    :return: Premarket candles, Regular Market, After hours
+    """
+    premarket = []
+    regularMarket = []
+    afterhour = []
+
+    for candle in data['candles']:
+        if candle['time'] < datetime.time(9, 30):
+            premarket.append(candle)
+        elif candle['time'] < datetime.time(16,0):
+            regularMarket.append(candle)
+        else:
+            afterhour.append(candle)
+
+    return premarket, regularMarket, afterhour
+
+def getYearlyDataTD(symbol, startDate, endDate):
     priceHistory = "https://api.tdameritrade.com/v1/marketdata/{symbol}/pricehistory".format(symbol=symbol)
     dailyChartPayLoad = {
         'apikey': TD_API['API_KEY'],
@@ -62,6 +103,144 @@ def getStockDataTD(symbol, startDate, endDate):
     data = pandas.DataFrame(data['candles'])
     data.columns = data.columns.str.capitalize()
     return data
+
+def getTDData(ticker,start,end):
+    try:
+        stockData = getYearlyDataTD(ticker,
+                                    datetime.datetime.strptime(start, '%Y-%m-%d'),
+                                    datetime.datetime.strptime(end, '%Y-%m-%d'))
+
+        stockData.insert(loc=0, column='Counter', value=np.arange(len(stockData)))
+        stockData = stockData.reset_index()
+        stockData = stockData.astype({'Date': 'datetime64'})
+        stockData = stockData.set_index('Date')
+        stockData = stockData.sort_values(by='Date')
+        return stockData
+    except RemoteDataError:
+        print('No Data found for {0}'.format(ticker))
+
+def getData(ticker, start, end):
+    try:
+        stockData = data.DataReader(ticker,
+                                    'stooq',
+                                    start,
+                                    end)
+
+        stockData.insert(loc=0, column='Counter', value=np.arange(len(stockData)))
+        stockData = stockData.reset_index()
+        stockData = stockData.astype({'Date': 'datetime64'})
+        stockData = stockData.set_index('Date')
+        stockData = stockData.sort_values(by='Date')
+        return stockData
+    except RemoteDataError:
+        print('No Data found for {0}'.format(ticker))
+
+# ====== Fetch Statistics Data ======== #
+
+def getPremarketData(data):
+    """
+    Get Premarket Highest price, Lowest price after Highest price is met, the respective times and cumulative volume
+    :param data: Stock Candles Data
+    :return: Premarket High, Premarket High Time, Premarket Low after High, Low time and cumulative volume
+    """
+    premarketData = {
+        'Premarket High': 0.0,
+        'Premarket High Time': datetime.datetime.now(),
+        'Premarket Low (After Top Reached)' : 0.0,
+        'Premarket Low Time (After Top Reached)' : datetime.datetime.now(),
+        'Premarket Volume':0
+    }
+
+    for candle in data:
+        if candle['high'] > premarketData['Premarket High']:
+            premarketData['Premarket High'] = candle['high']
+            premarketData['Premarket High Time'] = candle['time']
+            premarketData['Premarket Low (After Top Reached)'] = candle['high']
+        elif candle['low'] < premarketData['Premarket Low (After Top Reached)']:
+            premarketData['Premarket Low (After Top Reached)'] = candle['low']
+            premarketData['Premarket Low Time (After Top Reached)'] = candle['time']
+
+        premarketData['Premarket Volume'] += candle['volume']
+
+    return premarketData
+
+def getPivotPoints(df):
+    """
+    Get pivot points in stock chart
+    Swing lows are reversals when the bars go from red to green
+    Swing highs are reversals when bars go from green to red
+    :param df: Stock Chart
+    :return:
+    """
+    resistancePivots = []
+    supportPivots = []
+    prevBarIsGreen = df['Close'].iloc[0] > df['Open'].iloc[0]
+    prevClose = df['Close'].iloc[0]
+    print(prevBarIsGreen)
+
+    for index, row in df.iloc[1:].iterrows():
+        if row['Close'] > row['Open'] and prevBarIsGreen == False:
+            num = prevClose if prevClose < row['Open'] else row['Open']
+            supportPivots.append((index.to_pydatetime(),num, row['Counter'])) # Note: You could have prev red candle as start of pivot (OR?)
+            prevBarIsGreen = True
+        elif row['Close'] < row['Open'] and prevBarIsGreen:
+            num = prevClose if prevClose > row['Open'] else row['Open']
+            resistancePivots.append((index.to_pydatetime(),num, row['Counter']))
+            prevBarIsGreen = False
+        prevClose = row['Close']
+
+    return supportPivots, resistancePivots
+
+class Trendline():
+    def __init__(self, startPivot, endPivot, pivots):
+        self.startPivot = startPivot
+        self.endPivot = endPivot
+        self.pivots = pivots
+        self.slope, self.yIntercept = math_calcs.getLineGraph(startPivot[2], startPivot[1], endPivot[2], endPivot[1])
+        self.marginOfError = 0.1
+        self.score = 0
+
+    def calcTouchPoints(self):
+        touchPoint = 0
+        for p in self.pivots:
+            if abs(self.yIntercept + self.slope * p[2] - p[1]) < ((self.yIntercept + self.slope * p[2]) * self.marginOfError):
+                touchPoint += 1
+
+        self.score = touchPoint
+
+        return touchPoint
+
+def generateTrendLine(pivots, startTime=0, endTime=0, reverse=False):
+    """
+    Sort the pivots by ascending or descending order
+    Graph a line between the 2 points and check how many pivot points touch it
+    Count how many range exceeds or is close enough
+    1. Ignore dates outside of pivot range
+    :param pivots:
+    :param startTime:
+    :param endTime:
+    :return:
+    """
+    sortedPivots = sorted(pivots,key=lambda x:x[1],reverse=reverse)
+    p1 = sortedPivots[0]
+    prevDate = p1[0]
+    highScore = 0
+    pair = {}
+
+    for piv in sortedPivots[1:]:
+        if not (prevDate > piv[0]):
+            trendPoint = Trendline(p1, piv, sortedPivots).calcTouchPoints()
+            if highScore < trendPoint:
+                highScore = trendPoint
+                pair = {
+                        'Date1': p1[0], # .strftime("%Y-%m-%d"),
+                        'Pivot1': p1[1],
+                        'Date2': piv[0], # .strftime("%Y-%m-%d"),
+                        'Pivot2': piv[1]
+                    }
+                prevDate = pair['Date2']
+
+    return pair
 
 def getAllStocks():
     """
@@ -110,18 +289,34 @@ def updateStockInfo(df, date):
             print('No Data found for {0}'.format(index))
     return [df.to_dict()]
 
+def getStockList(storeExcel=False, path=None):
+    """
+    Get Stock information from Finviz
+    :param storeExcel: Store data into excel or not
+    :param path: location to store excel file
+    :return:
+    """
+    stockList = Screener()
+    stockList = pandas.DataFrame(stockList)
+    if storeExcel:
+        stockList.to_excel(path)
+
+    return stockList
+
+
 def readStockData(symbol):
     df = pandas.read_excel('D:/The Fastlane Project/Coding Projects/Stock Analysis/results/stock_data/{0}_Data.xlsx'.format(symbol), index_col=0)
     return df
 
 if __name__ == '__main__':
-    # print("Getting all stocks")
-    dateTimeStrStart = '2021-1-2 16:00'
-    dateTimeStrEnd = '2021-7-8 16:00'
+    print("Getting all stocks")
+
+    dateTimeStrStart = '2021-8-20 9:30'
+    dateTimeStrEnd = '2021-8-20 16:00'
     dateTimeStart = datetime.datetime.strptime(dateTimeStrStart, '%Y-%m-%d %H:%M')
     dateTimeEnd = datetime.datetime.strptime(dateTimeStrEnd, '%Y-%m-%d %H:%M')
 
-    print(getStockDataTD('NIO', dateTimeStart, dateTimeEnd))
+    print(getDailyDataTD('NIO', dateTimeStart, dateTimeEnd))
 
 
     ### PARALLEL PROCESSING
